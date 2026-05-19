ff44aff8de970f7416c4e21aa3266bcbb2cd891c
Ronjo33 <Zlatomir.stefanov480@edu1.pgzaimov.com>
2026-05-19 12:31:16 +0300

    krai

---
+++ AvtoSianieASP/Data/Migrations/20260519092620_FixCleanDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AvtoSianieASP.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCleanDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReservationDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReservationTime",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReservationDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ReservationTime",
+                table: "Orders");
+        }
+    }
+}
---
+++ AvtoSianieASP/SQLQuery1.sql
@@ -0,0 +1,19 @@
+﻿IF NOT EXISTS (SELECT 1 FROM Categories WHERE Name = N'Детайлинг')
+BEGIN
+    INSERT INTO Categories (Name, DateOn)
+    VALUES (N'Детайлинг', GETDATE());
+END
+
+DECLARE @CategoryId INT;
+
+SELECT @CategoryId = Id
+FROM Categories
+WHERE Name = N'Детайлинг';
+
+INSERT INTO Serveces
+(KatNum, DescSurves, CategoryId, Equipment, Duration, Image, Price, DateOn)
+VALUES
+(N'USL-001', N'Външно измиване', @CategoryId, 1, 30, N'/images/services/ChatGPT Image May 9, 2026, 05_28_54 PM.png', 20, GETDATE()),
+(N'USL-002', N'Вътрешно почистване', @CategoryId, 2, 60, N'/images/services/ChatGPT Image May 9, 2026, 05_23_43 PM.png', 40, GETDATE()),
+(N'USL-003', N'Пастиране и полиране', @CategoryId, 3, 180, N'/images/services/ChatGPT Image May 9, 2026, 05_25_07 PM.png', 120, GETDATE()),
+(N'USL-004', N'Пълен детайлинг', @CategoryId, 4, 480, N'/images/services/ChatGPT Image May 9, 2026, 05_27_24 PM.png', 250, GETDATE());
